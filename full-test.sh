89aed7d45f492cc616dadfb5c446713f913fbd33
Jordan Gamache <jordangamache115@gmail.com>
2026-08-02 22:34:26 -0700

    Fixed kernel panic initramfs issues!

--- full-test.sh
+++ full-test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # This script can be copied into your base directory for use with
 # automated testing using assignment-autotest.  It automates the
 # steps described in https://github.com/cu-ecen-5013/assignment-autotest/blob/master/README.md#running-tests
--- unit-test.sh
+++ unit-test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Run unit tests for the assignment
 
 # Automate these steps from the readme:
